f06a2b5531a0d5b3c5fc4b950caf4336da256fdb
abhijeetchaubey <chaubeyabhijeet36@gmail.com>
2025-07-01 15:10:01 +0530

    Fix refreshToken logic to handle missing token case and add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -175,7 +175,7 @@ const logOutUSer = asyncHandeler(async(req,res)=>{
 const refreshToken = asyncHandeler(async(req,res)=>{
   const incomingRefreshToken =req.cookies.refreshToken || req.body.refreshToken
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401,"Unauthorized Request")
   }
 
